339f60acd90927e985bd0a8b18a84d6e84c03927
Edie Pasek <pasekei@gmail.com>
2025-06-17 10:43:08 -0500

    Clean Up Header and Footer

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,8 +1,12 @@
 <!-- FOOTER: DEBUG INFO + COPYRIGHTS -->
-
 <footer>
+  <div class="environment">
+    <p>BG3 Mod Manager Linux Helper | <?= ENVIRONMENT ?> Environment</p>
+  </div>
+  <div class="copyrights">
+    <p>&copy; <?= date('Y') ?> Wushin. All rights reserved.</p>
+  </div>
 </footer>
-
 <!-- SCRIPTS -->
 <script type="text/javascript" src="<?php echo base_url().'js/bg3.js'; ?>"></script>
 <!-- -->
