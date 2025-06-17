0e4e2e15015ea6d58324836d88a1e00fd26ba7ce
Edie Pasek <pasekei@gmail.com>
2025-06-17 11:34:11 -0500

    Clean Up CSS formatting

--- app/Views/mods/index.php
+++
@@ -1,5 +0,0 @@
-<div class="heroe">
-    <?php foreach ($dirs as $dir): ?>
-        <p><a href="<?= esc($dir) ?>"><?= esc($dir) ?></p>
-    <?php endforeach ?>
-</div>
--- app/Views/mods/show.php
+++ app/Views/mods/show.php
@@ -1,29 +1,37 @@
-<input type="hidden" id="path" value="<?php echo $bginfo['path'] ?>"/>
+<input type="hidden" id="path" value="<?= esc($bginfo['path']) ?>" />
+
 <div class="row">
   <div class="column left">
-    <ul>
-      <li>Search:</li>
-      <li><input id="search"></input><button onclick="search('search')">Search</button><button onclick="clearInput('search')">Clear</button></li>
-    </ul>
-    <ul>
-      <li>Replace:</li>
-      <li><input id="replace"></input><button onclick="replace('search','replace')">Replace</button><button onclick="clearInput('replace')">Clear</button></li>
-    </ul>
-    <ul>
-      <?php if ($Mods !== ""): ?>
-    <ul id="myUL">
-        <li>Files:</li>
-        <?php echo $Mods; ?>
-    </ul>
-      <?php else: ?>
-    <ul>
-        <p>Unable to find any Mod Files for you.</p>
-    </ul>
-      <?php endif ?>
+    <div class="form-row">
+      <label for="search">Search:</label>
+      <input id="search" />
+      <button onclick="search('search')">Search</button>
+      <button onclick="clearInput('search')">Clear</button>
+    </div>
+
+    <div class="form-row">
+      <label for="replace">Replace:</label>
+      <input id="replace" />
+      <button onclick="replace('search','replace')">Replace</button>
+      <button onclick="clearInput('replace')">Clear</button>
+    </div>
+
+    <div class="file-list">
+      <strong>Files:</strong>
+      <ul id="myUL">
+        <?php if (!empty($Mods)): ?>
+          <?= $Mods ?>
+        <?php else: ?>
+          <li><em>Unable to find any Mod Files for you.</em></li>
+        <?php endif ?>
+      </ul>
+    </div>
   </div>
+
   <div class="column middle">
     <div id="displayDiv"></div>
   </div>
+
   <div class="column right">
     <div id="searchDiv"></div>
   </div>
